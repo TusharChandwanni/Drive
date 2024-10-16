9d28661e0deb3f366d7d88ca89515ad10a4e5128
Tushar Chandwanni <97699528+TusharChandwanni@users.noreply.github.com>
2024-10-17 01:43:54 +0530

    Update SignUpForm.jsx

--- client/src/Components/SignUp/SignUpForm/SignUpForm.jsx
+++ client/src/Components/SignUp/SignUpForm/SignUpForm.jsx
@@ -70,7 +70,11 @@ export default function SignUpForm() {
 
     const handleSubmit = async(event) => {
         event.preventDefault();
-        
+        if(!isValidEmail(formData.email))
+        {
+            return window.alert('Invalid email entered');
+        }
+
         
         const newFormData={...formData,picturePath:picturePath?picturePath:null,picture:pictureFile?pictureFile[0]:null}
 
